2783359d7a6a9953be872cf423fa07d7952d92ab
sundarmadesh <sundarmadesh13@gmail.com>
2024-04-25 12:08:43 +0530

    New code added by Raja

--- src/test/java/org/test/teststepblaze.java
+++ src/test/java/org/test/teststepblaze.java
@@ -1,16 +1,29 @@
 package org.test;
 
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import org.report.JvmReport;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\Sundar\\eclipse-workspace\\blazedemocucumber\\src\\test\\resources\\blazedemo\\blaze.feature",
-glue = "org.stepblazedemo", monochrome = true, strict = false, dryRun = false)
+@CucumberOptions(features = "F:\\Automation Testing\\Clonefolder\\SundarCode\\src\\test\\resources\\blazedemo\\blaze.feature",
+glue = "org.stepblazedemo", monochrome = true, strict = false, dryRun = false, plugin = {
+"json:F:\\Automation Testing\\Clonefolder\\SundarCode\\Reports\\Json\\report.json"})
 
-public class teststepblaze {
+public class teststepblaze  {
 	
-	
+			@AfterClass
+			public static void report() {
+			
+				JvmReport.reportjvm("F:\\\\Automation Testing\\\\Clonefolder\\\\SundarCode\\\\Reports\\\\Json\\\\report.json");
+				
 
-}
+			}
+
+	}
+
+
